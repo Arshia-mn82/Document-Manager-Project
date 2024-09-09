01d573d9456e62ce881c5525b7fc8047025973a3
Arshia <arshiamotaghian@yahoo.com>
2024-09-09 21:32:43 +0330

    Changes to the Views using generics library and the needed urls

--- document_manager/document/urls.py
+++ document_manager/document/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("list/", FileListView.as_view()),
-    path("upload/", FileUploadView.as_view()),
+    path("list/", FileListCreateView.as_view()),
+    path("upload/", FileListCreateView.as_view()),
     path("token/", Login.as_view()),
     path("token/refresh/", Refresh.as_view()),
 ]
--- document_manager/document/views.py
+++ document_manager/document/views.py
@@ -1,80 +1,44 @@
-from .serializer import *
-from .models import *
-from rest_framework.views import APIView
+from .serializer import TextFileSerializer
+from .models import TextFile
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import FileUploadParser
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 
 class Login(TokenObtainPairView):
     pass
 
-
 class Refresh(TokenRefreshView):
     pass
 
-
-class FileListView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        files = TextFile.objects.filter(user=request.user)
-        serializer = TextFileSerializer(files, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class FileUploadView(APIView):
+# List and Create View for File Management
+class FileListCreateView(ListCreateAPIView):
+    serializer_class = TextFileSerializer
     permission_classes = [IsAuthenticated]
     parser_classes = [FileUploadParser]
 
-    def post(self, request):
-        file_serializer = TextFileSerializer(data=request.data)
-        if file_serializer.is_valid():
-            file_serializer.save(user=request.user)
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        # Filter files by the authenticated user
+        return TextFile.objects.filter(user=self.request.user)
 
+    def perform_create(self, serializer):
+        # Save file with the user who is authenticated
+        serializer.save(user=self.request.user)
 
-class FileDownloadView(APIView):
+# Retrieve, Update, and Delete View for Single File Operations
+class FileRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TextFileSerializer
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        # Filter by authenticated user to ensure security
+        return TextFile.objects.filter(user=self.request.user)
 
-    def get(self, request, pk):
-        file = get_object_or_404(TextFile, pk=pk, user=request.user)
-        response = FileResponse(file.file.open("rb"))
-        return response
+    def retrieve(self, request, *args, **kwargs):
+        # Custom retrieve logic for downloading the file
+        file = self.get_object()
+        return FileResponse(file.file.open("rb"))
 
-
-class FileDownloadView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, pk):
-        file = get_object_or_404(TextFile, pk=pk, user=request.user)
-        response = FileResponse(file.file.open("rb"))
-        return response
-
-
-class FileEditView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def put(self, request, pk):
-        file = get_object_or_404(TextFile, pk=pk, user=request.user)
-        file_serializer = TextFileSerializer(file, data=request.data, partial=True)
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_200_OK)
-        return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class FileDeleteView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def delete(self, request, pk):
-        file = get_object_or_404(TextFile, pk=pk, user=request.user)
-        file.file.delete()
-        file.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
